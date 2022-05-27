c00843519b0d8de44d3b7e1e3efcf246345f3538
Kartik Mehta <kartikmehta202@gmail.com>
2022-05-27 20:41:06 +0530

    Front Initial

---
+++ FRONTEND/src/components/App.js
@@ -0,0 +1,37 @@
+import { useEffect } from "react";
+import Signup from "./Signup";
+import Login from "./Login";
+import ForgotPassword from "./ForgotPassword";
+import { AuthProvider } from "../contexts/AuthContext";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import PrivateRoute from "./PrivateRoute";
+import alanBtn from "@alan-ai/alan-sdk-web";
+
+export default function App() {
+    useEffect(() => {
+        alanBtn({
+            key: "df5e0a87ce4cf578d610608f6aad42102e956eca572e1d8b807a3e2338fdd0dc/stage",
+            onCommand: (commandData) => {
+                if (commandData.command === "go:back") {
+                    // Call the client code that will react to the received command
+                }
+            },
+        });
+    }, []);
+
+    return (
+        <Router>
+            <AuthProvider>
+                <Routes>
+                    <Route exact path="/" element={<PrivateRoute />} />
+                    <Route path="/signup" element={<Signup />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route
+                        path="/forgot-password"
+                        element={<ForgotPassword />}
+                    />
+                </Routes>
+            </AuthProvider>
+        </Router>
+    );
+}
